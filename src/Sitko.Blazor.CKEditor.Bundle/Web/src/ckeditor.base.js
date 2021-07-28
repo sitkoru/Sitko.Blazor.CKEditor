a9f8c1fedcf34772eab65863eab25c4a4a33f6f9
George Drak <sonicgd@gmail.com>
2021-07-28 15:36:36 +0500

    Upgrade ckeditor build with more features

--- src/Sitko.Blazor.CKEditor.Bundle/Web/src/ckeditor.base.js
+++ src/Sitko.Blazor.CKEditor.Bundle/Web/src/ckeditor.base.js
@@ -4,51 +4,119 @@
  */
 
 // The editor creator to use.
-import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
+import AutoLink from '@ckeditor/ckeditor5-link/src/autolink.js';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code.js';
+import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock.js';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import GeneralHtmlSupport from '@ckeditor/ckeditor5-html-support/src/generalhtmlsupport.js';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
+import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+import Link from '@ckeditor/ckeditor5-link/src/link.js';
+import List from '@ckeditor/ckeditor5-list/src/list.js';
+import Markdown from '@ckeditor/ckeditor5-markdown-gfm/src/markdown.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat.js';
+import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting.js';
+import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharacters.js';
+import SpecialCharactersArrows from '@ckeditor/ckeditor5-special-characters/src/specialcharactersarrows.js';
+import SpecialCharactersCurrency from '@ckeditor/ckeditor5-special-characters/src/specialcharacterscurrency.js';
+import SpecialCharactersEssentials from '@ckeditor/ckeditor5-special-characters/src/specialcharactersessentials.js';
+import SpecialCharactersText from '@ckeditor/ckeditor5-special-characters/src/specialcharacterstext.js';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough.js';
+import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript.js';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript.js';
+import Table from '@ckeditor/ckeditor5-table/src/table.js';
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
+import WordCount from '@ckeditor/ckeditor5-word-count/src/wordcount.js';
 
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-import Indent from '@ckeditor/ckeditor5-indent/src/indent';
-import Link from '@ckeditor/ckeditor5-link/src/link';
-import List from '@ckeditor/ckeditor5-list/src/list';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import GeneralHtmlSupport from '@ckeditor/ckeditor5-html-support/src/generalhtmlsupport';
-import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting';
-
-export default class BlazorEditor extends ClassicEditorBase {
+// Plugins to include in the build.
+class BlazorEditor extends ClassicEditor {
 }
 
 // Plugins to include in the build.
 BlazorEditor.builtinPlugins = [
-    Essentials,
-    Bold,
-    Italic,
-    Heading,
-    Indent,
-    Link,
-    List,
-    Paragraph,
-    GeneralHtmlSupport,
-    SourceEditing
+  Alignment,
+  Autoformat,
+  AutoLink,
+  Bold,
+  Code,
+  CodeBlock,
+  Essentials,
+  GeneralHtmlSupport,
+  Heading,
+  HorizontalLine,
+  Indent,
+  Italic,
+  Link,
+  List,
+  Markdown,
+  Paragraph,
+  RemoveFormat,
+  SourceEditing,
+  SpecialCharacters,
+  SpecialCharactersArrows,
+  SpecialCharactersCurrency,
+  SpecialCharactersEssentials,
+  SpecialCharactersText,
+  Strikethrough,
+  Subscript,
+  Superscript,
+  Table,
+  TableToolbar,
+  TextTransformation,
+  Underline,
+  WordCount
 ];
 
 // Editor configuration.
 BlazorEditor.defaultConfig = {
-    toolbar: {
-        items: [
-            'heading',
-            '|',
-            'bold',
-            'italic',
-            'link',
-            'bulletedList',
-            'numberedList',
-            'undo',
-            'redo'
-        ]
-    },
-    //This value must be kept in sync with the language defined in webpack.config.js.
-    language: 'en'
+  toolbar: {
+    items: [
+      'sourceEditing',
+      '|',
+      'undo',
+      'redo',
+      '|',
+      'heading',
+      '|',
+      'bold',
+      'italic',
+      'strikethrough',
+      'underline',
+      'subscript',
+      'superscript',
+      'alignment',
+      '|',
+      'bulletedList',
+      'numberedList',
+      '|',
+      'insertTable',
+      'link',
+      'code',
+      'codeBlock',
+      'horizontalLine',
+      'specialCharacters',
+      '|',
+      'removeFormat'
+    ]
+  },
+  table: {
+    contentToolbar: [
+      'tableColumn',
+      'tableRow',
+      'mergeTableCells'
+    ]
+  },
+  //This value must be kept in sync with the language defined in webpack.config.js.
+  language: 'en'
 };
+
+export default BlazorEditor;
--- src/Sitko.Blazor.CKEditor.Bundle/Web/webpack.config.js
+++ src/Sitko.Blazor.CKEditor.Bundle/Web/webpack.config.js
@@ -14,97 +14,95 @@ const CKEditorWebpackPlugin = require('@ckeditor/ckeditor5-dev-webpack-plugin');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
 
 const config = {
-    performance: {hints: false},
-    entry: {
-        "ckeditor": path.resolve(__dirname, 'src', 'ckeditor.js'),
-        "ckeditor.dark": path.resolve(__dirname, 'src', 'ckeditor.dark.js')
-    },
-    output: {
-        // The name under which the editor will be exported.
-        library: 'BlazorEditor',
+  performance: {hints: false},
+  entry: {
+    "ckeditor": path.resolve(__dirname, 'src', 'ckeditor.js'),
+    "ckeditor.dark": path.resolve(__dirname, 'src', 'ckeditor.dark.js')
+  },
+  output: {
+    // The name under which the editor will be exported.
+    library: 'BlazorEditor',
 
-        path: path.resolve(__dirname, '..', 'wwwroot'),
-        filename: '[name].js',
-        libraryTarget: 'umd',
-        libraryExport: 'default'
-    },
+    path: path.resolve(__dirname, '..', 'wwwroot'),
+    filename: '[name].js',
+    libraryTarget: 'umd',
+    libraryExport: 'default'
+  },
 
-    plugins: [
-        new CKEditorWebpackPlugin({
-            // UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
-            // When changing the built-in language, remember to also change it in the editor's configuration (src/ckeditor.js).
-            language: 'ru',
-            addMainLanguageTranslationsToAllAssets: true,
-            additionalLanguages: []
-        }),
-        new webpack.BannerPlugin({
-            banner: bundler.getLicenseBanner(),
-            raw: true
-        })
-    ],
+  plugins: [
+    new CKEditorWebpackPlugin({
+      // UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
+      // When changing the built-in language, remember to also change it in the editor's configuration (src/ckeditor.js).
+      language: 'en',
+      addMainLanguageTranslationsToAllAssets: true,
+      additionalLanguages: 'all'
+    }),
+    new webpack.BannerPlugin({
+      banner: bundler.getLicenseBanner(),
+      raw: true
+    })
+  ],
 
-    module: {
-        rules: [
-            {
-                test: /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
-                use: [ 'raw-loader' ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                        options: {
-                            injectType: 'singletonStyleTag',
-                            attributes: {
-                                'data-cke': true
-                            }
-                        }
-                    },
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: styles.getPostCssConfig({
-                                themeImporter: {
-                                    themePath: require.resolve('@ckeditor/ckeditor5-theme-lark')
-                                },
-                                minify: true
-                            })
-                        }
-                    },
-
-                ]
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        use: ['raw-loader']
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: {
+              injectType: 'singletonStyleTag',
+              attributes: {
+                'data-cke': true
+              }
             }
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: styles.getPostCssConfig({
+                themeImporter: {
+                  themePath: require.resolve('@ckeditor/ckeditor5-theme-lark')
+                },
+                minify: true
+              })
+            }
+          },
         ]
-    }
+      }
+    ]
+  }
 };
 
 module.exports = (env, argv) => {
-    if (argv.mode === 'development') {
-        config.devtool = 'source-map';
+  if (argv.mode === 'development') {
+    config.devtool = 'source-map';
+  }
+
+  if (argv.mode === 'production') {
+    config.optimization = {
+      minimizer: [
+        new TerserWebpackPlugin({
+          sourceMap: true,
+          terserOptions: {
+            output: {
+              // Preserve CKEditor 5 license comments.
+              comments: /^!/
+            }
+          },
+          extractComments: false
+        })
+      ]
     }
-
-    if (argv.mode === 'production') {
-        config.optimization = {
-            minimize: true,
-            minimizer: [
-                new TerserWebpackPlugin({
-                    terserOptions: {
-                        compress: {
-                            drop_console: true, // will remove console.logs from your files
-                        },
-                        output: {
-                            // Preserve CKEditor 5 license comments.
-                            comments: /^!/
-                        },
-                    },
-                    extractComments: false
-                }),
-            ],
-        }
-    }
+  }
 
 
-    return config;
+  return config;
 };
