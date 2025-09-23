b16d8c33f50c155731b994231ea21896d18107d0
SonicGD <sonicgd@gmail.com>
2025-09-23 19:24:11 +0500

    feat: allow to load multiple css files

--- src/Sitko.Blazor.CKEditor/CKEditorOptions.cs
+++ src/Sitko.Blazor.CKEditor/CKEditorOptions.cs
@@ -6,7 +6,7 @@ using System.ComponentModel.DataAnnotations;
 public class CKEditorOptions
 {
     [Required] public virtual string ScriptPath { get; set; } = "";
-    [Required] public virtual string StylePath { get; set; } = "";
+    [Required] public virtual Dictionary<string, string> StylePaths { get; set; } = [];
     public virtual Dictionary<string, string> GetAdditionalScripts(CKEditorConfig? config) => new();
     [Required] public virtual string EditorClassName { get; set; } = "";
     public virtual CKEditorConfig? CKEditorConfig { get; set; }
